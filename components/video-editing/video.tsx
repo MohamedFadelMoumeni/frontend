074259165ec5bb41a178c4a5b300402d6c66b746
medinvygo <mohamed.fadel@invygo.com>
2024-08-10 17:14:38 +0100

    fix responsive

--- components/video-editing/video.tsx
+++ components/video-editing/video.tsx
@@ -43,8 +43,8 @@ const Video = (props : Props) => {
         {isMuted ? <IoVolumeMute size={20} className='text-white'/> : <IoVolumeHigh size={20} className='text-white' />}
       </div>
     }
-   <video  src={props.url} onClick={handlePlayPause} onMouseLeave={() => setHover(false)} onMouseEnter={() => setHover(true)} ref={videoRef}  playsInline className='rounded-3xl h-full w-full object-cover absolute top-0 right-0 left-0 bottom-0'>
-   </video>
+    <video  src={props.url} onClick={handlePlayPause} onMouseLeave={() => setHover(false)} onMouseEnter={() => setHover(true)} ref={videoRef}  playsInline className='rounded-3xl h-full w-full object-cover absolute top-0 right-0 left-0 bottom-0 '>
+    </video>
    </>
   )
 }
